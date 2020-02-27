405c9f1ed8a2421935a2ef06218467784571cc51
1_Hours <sharipov.yanepenb@gmail.com>
2020-02-27 23:24:13 +0700

    layouts

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,12 @@
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel</p>
+<!-- Хранится в resources/views/about.blade.php -->
+
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel</p>
+@endsection
--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,1 +1,11 @@
-<h1>Статьи</h1>
+<!-- Хранится в resources/views/about.blade.php -->
+
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'Статьи')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>Статьи</h1>
+@endsection
